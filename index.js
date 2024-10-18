5ee80a93d6d84867b6428daf331e170cb1fe6362
ThouaibatSelim <selimthouaibat9@gmail.com>
2024-10-18 18:02:16 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,38 @@
+//Jeu de calcul
+
+let année = 2024;
+
+
+//Objet
+
+let user = { 
+nom: "SELIM",
+prenom: "Thouaïbat",
+naissance: 2002
+}    ;
+
+
+// Fonction
+
+let currentYear = 2024;
+let birthYear = 2002;
+
+function diff (currentYear, birthYear) {
+    let resultat = currentYear - birthYear
+    return resultat
+};
+
+let reduction = (2002, 2024);
+
+//Affichage
+console.log (resultat);
+
+//DOM
+let elementAge = document.querySelector ("#age1");
+elementAge.setAttribute ("value", "22");
+
+
+
+
+
+
