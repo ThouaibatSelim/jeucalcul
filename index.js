898c1b76253dee605433ead9a80b88a4af3470ed
Espace NUMERIQUE 12 <espace12@numerique.lan>
2024-10-22 15:57:20 +0300

    display of age

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 //Jeu de calcul
 
-let année = 2024;
-
-
 //Objet
 
 let user = { 
@@ -10,7 +7,6 @@ nom: "SELIM",
 prenom: "Thouaïbat",
 naissance: 2002
 } ;
-
 
 // Fonction
 
@@ -23,43 +19,66 @@ function diff (currentYear, birthYear) {
     return reduction;
 };
 
-
 //DOM
 
-// elementName.setAttribute ("value", "SELIM")
 
-// let elementPrenom = document.querySelector ("#n2");
-// elementPrenom.setAttribute ("value", "Thouaïbat")
+// STOP RESET
+document.querySelector("#bouton").addEventListener("click", function(event) {
+    // Empêche le bouton de faire quoi que ce soit d'autre
+    event.preventDefault();
+    
+});
 
-// let elementAge = document.querySelector ("#age1");
-// elementAge.setAttribute ("value", 22);
+
+// ///////////
 
 let elementYear = document.querySelector ("#year");
-
-
-
-// elementYear.setAttribute ("value", "2002");
-
 let elementValider = document.querySelector ("#bouton") ;
+
 console.log (elementValider);
+
+//Evenement
 
 elementValider.addEventListener ("click", () => {
     console.log("Vous avez cliqué sur le bouton Valider.");
     let elementBirthyear = elementYear.value;
-
-    
-    // let anneeSaisie = elementBirthyear.value;
     console.log ("Année saisie:", elementBirthyear);
     
-    diff (2024, elementBirthyear);
+   const age = diff (2024, elementBirthyear);
+
+    //Nom
+    let elementNom = document.querySelector ("#n1");
+    let elementlastname = elementNom.value;
+    console.log ("Nom saisi:", elementlastname);
     
-    let elementNom = document.querySelector ("#n1")
-    console.log ("Nom saisi:", elementNomdef);
-    let elementNomdef = elementNom.value;
+    //Prénom
+    let elementPrenom = document.querySelector ("#prenom");
+    let elementFirstName = elementPrenom.value;
+    console.log("Prénom saisi:", elementFirstName);
     
-})
+    //Span
+    const elementSpan = document.querySelector("#messageage");
+    elementSpan.innerHTML += ` Vous avez ${age} ans, `;
 
-   
+    if (age < 18) {
+        elementSpan.innerHTML += "vous êtes mineur(e).";
+    } else { elementSpan.innerHTML +="vous êtes majeur(e)";
 
+    }
 
+    //Situation
+    const elementPro = document.querySelectorAll('input[name="situation"]');
+    console.log(elementPro);
 
+    let checked;
+    for (let i = 0; i < elementPro.length ; i++) {
+
+    if (elementPro[i].checked){
+        console.log (elementPro[i].value);
+        checked = elementPro[i].value;
+        break
+    } 
+}
+
+elementSpan.innerHTML += ` et vous êtes ${checked}.`;
+});
